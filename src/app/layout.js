a93482a98f609121c411dbdfa1cbdb151c955e65
CloakedZay <zaywhy.2020@gmail.com>
2026-04-26 23:49:27 +0800

    Update page logic

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col"
+      suppressHydrationWarning={true}
+      >
         <AuthProvider>
           {children}
         </AuthProvider>
